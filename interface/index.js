10aef5801ee49269a2ef0b48da6b2c6c2fe67a5e
romakovich <88998053+romakovich@users.noreply.github.com>
2022-01-23 15:50:12 +0500

    feat: add youtube interface

--- interface/index.js
+++ interface/index.js
@@ -1,3 +1,3 @@
-clickToServer(".open-youtube", "isYoutubeOpen");
-clickToServer(".volume-up", "youtubeVolumeUp");
-clickToServer(".volume-down", "youtubeVolumeDown");
+// clickToServer(".open-youtube", "isYoutubeOpen");
+// clickToServer(".volume-up", "youtubeVolumeUp");
+// clickToServer(".volume-down", "youtubeVolumeDown");
