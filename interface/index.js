ac8531619b9d42b7d62f8fba013582cb8c66aa63
romakovich <88998053+romakovich@users.noreply.github.com>
2022-01-26 14:16:28 +0500

    feat: add layout general command

--- interface/index.js
+++ interface/index.js
@@ -43,6 +43,17 @@ timingHint.addEventListener("click", function() {
     }
 })
 
+const openGeneral = document.querySelector(".navbar__main");
+const generalCommand = document.querySelector(".general-container");
+openGeneral.addEventListener("click", function() { 
+    console.log("test")
+    if(generalCommand.classList.contains('active')) {
+        generalCommand.classList.remove('active')
+    } else {
+        generalCommand.classList.add('active')
+    }
+})
+
 clickToServer(".play-control__play", "youtubePlay");
 clickToServer(".volume-control__turn:nth-child(4)", "youtubeVolumeUp");
 clickToServer(".volume-control__turn:nth-child(2)", "youtubeVolumeDown");
@@ -50,11 +61,3 @@ clickToServer(".volume-control__turn:nth-child(2)", "youtubeVolumeDown");
 clickToServer(".play-control__left", "youtubeTimeLeft");
 clickToServer(".play-control__right", "youtubeTimeRight");
 clickToServer(".volume-control__full-screen", "youtubeFullScreen");
-
-// setInterval(() => {
-//     getStatus(".play-control__play")
-//     .then(data=>{
-//         console.log(data)
-//     })
-// }, 2000);
-
