5d184883ae8b7bc719160a5c69612de9bf691268
romakovich <88998053+romakovich@users.noreply.github.com>
2022-02-10 10:46:24 +0500

    test commit

--- express/src/scripts/fullResetDB.js
+++ express/src/scripts/fullResetDB.js
@@ -5,5 +5,5 @@ const fullResetDB = async () => {
     const collection = await getMongoDB('commands');
     await collection.updateOne({base: DB_NAME}, {$set: {"isYoutubeOpen":false,"youtubeVolumeUp":false,"youtubeVolumeDown":false,"isCompUnlock":false,"youtubePlay":false,"openVideo":false,"youtubeOpenVideo":false,"youtubeTimeLeft":false,"youtubeTimeRight":false,"youtubeFullScreen":false,"youtubeStatusPaused":"true","fullScreen":false,"browserZoomPlus":false,"browserZoomMinus":false,"browserTabClose":false,"browserTabLeft":false,"browserTabRight":false,"youtubeSubcriptions":false,"youtubeNumberVideo":false,"youtubeOpenVideoByNumber":false,"scrollDown":false,"scrollUp":false,"computerDisable":false}})
 }
-
+//test
 module.exports.fullResetDB = fullResetDB;
